4041843d36c579d4fcbf58683fc1fff188dcfcbf
VortexLite <minedinnerbone@gmail.com>
2023-12-13 08:50:57 +0200

    Add project

---
+++ OOPLaba1.2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPLaba1_2
+{
+    internal class Program
+    {
+        // константи на вагу
+        public const double butter1kg = 1;
+        public const double sourCream2kg = 2;
+        public const double cream3kg = 3;
+
+
+        static void Main(string[] args)
+        {
+            // кодировка
+            Console.OutputEncoding = Encoding.UTF8;
+            double butter, sourCream, cream, res;
+            butter = sourCream = cream = res = 0;
+
+            // обрахунок даних + сумма всього
+            Calculation(ref butter, ref sourCream, ref cream, ref res);
+
+            // вивід даних
+            Print(ref butter, ref sourCream, ref cream, ref res);
+        }
+
+        static void Calculation(ref double butter, ref double sourCream, ref double cream, ref double res)
+        {
+            butter = butter1kg * 8.50;
+            sourCream = sourCream2kg * 2.40;
+            cream = cream3kg * 4.10;
+
+            res = butter + sourCream + cream;
+        }
+
+        static void Print(ref double butter, ref double sourCream, ref double cream, ref double res)
+        {
+            Console.WriteLine($"Масло = {butter}\nСметана = {sourCream}\nВершки = {cream}\nЗагальна сумма = {res}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OOPLaba1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+
+namespace OOPLaba1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // кодировка
+            Console.OutputEncoding = Encoding.UTF8;
+            double x, y, z, res;
+
+            do
+            {
+                x = y = z = res = 0;
+
+                // заповнення даних
+                InputData(ref x, ref y, ref z);
+
+                // результат формули
+                res = Calculate(ref x, ref y, ref z);
+
+                // вивід даних
+                WriteRes(ref res, ref x, ref y, ref z);
+
+                if (x < 1)
+                {
+                    if (x < 10) Console.WriteLine("X < 10");
+                    else Console.WriteLine("X < 1");
+                }
+                else
+                {
+                    if (x > 10) Console.WriteLine("X > 10");
+                    else Console.WriteLine("X > 1");
+                }
+
+                if (res >= 0)
+                {
+                    if (res == 0) Console.WriteLine("Полупозитив - полунегатив");
+                    else Console.WriteLine("Позитивне");
+                }
+                else Console.WriteLine("Негативне");
+                
+
+            } while (x != 0);
+            Console.WriteLine("X = 0\nПрограмма завершена.");
+            Console.ReadLine();
+        }
+
+        static void InputData(ref double x, ref double y, ref double z)
+        {
+            Console.Write("Введіть x = ");
+            x = double.Parse(Console.ReadLine());
+
+            Console.Write("Введіть y = ");
+            y = double.Parse(Console.ReadLine());
+
+            Console.Write("Введіть z = ");
+            z = double.Parse(Console.ReadLine());
+        }
+
+        static double Calculate(ref double x, ref double y, ref double z)
+        {
+            return x * (Math.Atan(z) + Math.Pow(y, 3));
+        }
+
+        static void WriteRes(ref double res, ref double x, ref double y, ref double z)
+        {
+            Console.WriteLine($"X = {x}\t Y = {y}\t Z = {z}\nРезультат = {res}\n");
+        }
+    }
+}
